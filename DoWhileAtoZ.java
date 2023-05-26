58160079ca78726297510686ddefff3f569671c4
yoonsongjik <102634570+yoonsongjik@users.noreply.github.com>
2023-05-26 16:32:10 +0900

    DoWhileA-Z

---
+++ DoWhileAtoZ.java
@@ -0,0 +1,13 @@
+//¼Ò¹®ÀÚ a 97
+public class DoWhileAtoZ {
+	public static void main(String[] args)
+	{
+		int i = 97;
+		
+		do {
+			System.out.printf("%c",(char)i);
+			i++;
+		}
+		while(i<122);
+	}
+}
