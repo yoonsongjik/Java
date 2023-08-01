702d5ec5fab28826a5ce80d32b2d352fe6c4f783
윤성직 <102634570+yoonsongjik@users.noreply.github.com>
2023-08-01 10:27:41 +0900

    Add files via upload

---
+++ InputException.java
@@ -0,0 +1,26 @@
+//1761034 윤성직 9페이지
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class InputException {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("정수 3개를 입력하세요.");
+		int sum = 0, n = 0;
+		for(int i = 0; i<3; i++) {
+			System.out.print((i+1)+"번째 정수 : ");
+			try {
+				n = scanner.nextInt();
+			}
+			catch(InputMismatchException e) {
+				System.out.println("정수가 아닙니다 다시 입력하세요 : ");
+				scanner.next();
+				i--;
+				continue;
+			}
+			sum += n;
+		}
+		System.out.println("합은 :" + sum);
+		scanner.close();
+	}
+
+}
