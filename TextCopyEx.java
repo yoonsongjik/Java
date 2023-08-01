f242c5e0b327484ab91d8f4f3117f2249dbb0272
윤성직 <102634570+yoonsongjik@users.noreply.github.com>
2023-08-01 10:49:10 +0900

    Add files via upload

---
+++ TextCopyEx.java
@@ -0,0 +1,22 @@
+//1761034 윤성직 33페이지
+import java.io.*;
+
+public class TextCopyEx {
+    public static void main(String[] args) {
+        File src = new File("c:\\windows\\system.ini");
+        File dest = new File("c:\\Temp\\system.txt");
+        int c;
+        try {
+            FileReader fr = new FileReader(src);
+            FileWriter fw = new FileWriter(dest);
+            while ((c = fr.read()) != -1) {
+                fw.write((char) c);
+            }
+            fr.close();
+            fw.close();
+            System.out.println(src.getPath() + "를 " + dest.getPath() + "로 복사하였습니다");
+        } catch (IOException e) {
+            System.out.println("파일 복사 오류");
+        }
+    }
+}
