0c490f3ca89bfeaa5ecda29578e7d0ff9ca8c176
윤성직 <102634570+yoonsongjik@users.noreply.github.com>
2023-08-01 10:22:33 +0900

    Add files via upload

---
+++ EqualsEx.java
@@ -0,0 +1,30 @@
+class Point{
+	private int x, y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public boolean equals(Object obj)
+	{
+		Point p = (Point)obj;
+		if(x == p.x && y == p.y)
+			return true;
+		else 
+			return false;
+	}
+}
+public class EqualsEx {
+	public static void main(String[] args) {
+		Point a = new Point(2,3);
+		Point b = new Point(2,3);
+		Point c = new Point(3,4);
+		
+		if(a.equals(b))
+			System.out.println("a is equal to b");
+		if(a.equals(c))
+			System.out.println("a is equal to c");
+		if(b.equals(c))
+			System.out.println("b is equal to c");
+			}
+}
